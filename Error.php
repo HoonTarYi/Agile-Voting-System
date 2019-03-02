6ac914bfc8e7e3410fdf8f9e8bc8f839310ba08a
HoonTarYi <kaizukahayate@gmail.com>
2019-03-02 21:47:06 +0800

    Add Error Page - V3.2

---
+++ Error.php
@@ -0,0 +1,37 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php 
+require('Header.php');
+
+?>
+
+
+				<!-- Main -->
+					<article id="main">
+						<header>
+							<h2>Error</h2>
+							<p>You have no priority to in this page.</p>
+						</header>
+
+							<div class="inner">
+   
+
+
+
+   
+						
+				
+				
+	
+				<?php 
+require('Footer.php');
+
+?>
+        
+
+</body>
+
+</html>
---
+++ pollclose.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php 
+require('Header.php');
+
+?>
+
+
+				<!-- Main -->
+					<article id="main">
+						<header>
+							<h2>The Current Polls That You Wish Enter is Ended</h2>
+							<p>Please Wait New Polls to Vote.</p>
+						</header>
+
+							<div class="inner">
+   
+
+
+
+   
+						
+				
+				
+	
+				<?php 
+require('Footer.php');
+
+?>
+        
+
+</body>
+
+</html>
